eeec6f8e0a0898c22f840995a3210b05ca7fb591
magor2017 <magorsy3@gmail.com>
2019-07-10 00:43:56 +0100

    'first'

--- app/Controllers/paiement_marchandController.php
+++ app/Controllers/paiement_marchandController.php
@@ -9,11 +9,33 @@ use \App\Controller;
 
 
 class paiement_marchandController extends Controller {
+  private $bdd;
+  public function __construct(){
+    $this->bdd=new \pdo("mysql:host=localhost;dbname=mbirmiprod","root","");
+  }
+  public function closeConnection(){
+    $this->bdd=null;
+  }
 
   public function initier(Request $request, Response $response, $args){
     header("Access-Control-Allow-Origin: *");
-    $service = $request->getParsedBody()["service"];
-      return $response->withJson(array("idDem"=> 1, "codeMessage" =>"#123#")); 
+    $param = $request->getParsedBody();
+    if(isset($param["service"]) && isset($param["montant"])){
+      try{
+       // $bdd=new \pdo("mysql:host=localhost;dbname=mbirmiprod","root","");
+        $req=$this->bdd->prepare("INSERT INTO transaction(montant,services,etat) VALUES(:montant,:services,:etat)");
+        $status=$req->execute(array(":montant"=>$param["montant"],":services"=>$param["service"],":etat"=>1));
+       // closeConnection();
+        return $response->withJson(array("idDem"=> 1, "codeMessage" =>"#123#","status"=>$status),200); 
+      }catch(Exception $e){
+       // closeConnection();
+        return $response->withJson(array("status"=>false,"message"=>"problem de connection a la base de donnee"));
+      }
+    }else{
+     // closeConnection();
+      return $response->withJson(array("status"=>false,"message"=>"parametres incorrectes"));
+    }
+     
   } 
 
   	
---
+++ app/Models/PaimentMarchand.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Models;
+
+class PaimentMarchand{
+    private $prenom="magor";
+}
+
+?>
